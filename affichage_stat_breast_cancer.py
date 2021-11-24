894357e1ee12dc917d49b184f2a84e8780030a8e
SamuelNRS <samuel.nerisson@gmail.com>
2021-11-24 16:05:51 +0100

    Update affichage_stat_breast_cancer.py
    Ajout de l'algo KNN

--- affichage_stat_breast_cancer.py
+++ affichage_stat_breast_cancer.py
@@ -1,51 +1,82 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  3 16:17:13 2021
-
-@author: lulu
-"""
-
 import pandas as pd
-import statistics as s
-import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+#Ouvre le fichier csv
 data = pd.read_csv("./Breast_cancer Dataset/breast_cancer.csv",";", index_col="ID_REF")
-pd.set_option('display.max_columns', None)
-statN=data.describe()
-statC=data.describe(include=['object'])
 
-display(data.info())
-display(statN)
-display(statC)
+#Remplace les classifcation écrite en number
+#Intérêt ? Je sais pas
+ToReplace = ['breast cancer','non cancer','benign breast disease', 'prostate disease'] 
+Values = [0, 1, 2, 3]
+data['ETAT'] = data['ETAT'].replace(to_replace=ToReplace, value=Values)
 
-nrows = data.shape[0]
-rcols = data.shape[1]
+#x prends tout les columns en entrée, y tout les columns en sortie
+x=data.drop(columns=["ETAT"])
+y=data["ETAT"]
+#Récupère des entrées et des sorties d'entrainement et de test
+X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+#Paramètre l'algorithme de KNN avec une comparaison de trois voisin
+neigh = KNeighborsClassifier(n_neighbors=3)
+#Lance KNN avec les entrées et sorties d'entrainement
+neigh.fit(X_train, y_train)
+
+#Récupère les sorties prédites avec les entrées de test
+y_pred = neigh.predict(X_test)
+
+#Lance les différentes métric pour observer la justesse de l'algorithme
+result = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix :")
+print(result)
+result1 = classification_report(y_test, y_pred)
+print("Classification Report :")
+print(result1)
+result2 = accuracy_score(y_test, y_pred)
+print("Accuracy:", result2)
 
 
-data5898=data.groupby("ETAT")["MIMAT0005898"]
-data5951=data.groupby("ETAT")["MIMAT0005951"]
-data19691=data.groupby("ETAT")["MIMAT0019691"]
-data27623=data.groupby("ETAT")["MIMAT0027623"]
-data27650=data.groupby("ETAT")["MIMAT0027650"]
-
-display(data5898.describe())
-display(data5951.describe())
-display(data19691.describe())
-display(data27623.describe())
-display(data27650.describe())
-
-
-numerical_cols = data.select_dtypes(exclude=['object']).columns
-num_subplot_rows = len(numerical_cols)
-num_subplot_cols = 5
-plt.subplots(figsize=(5*num_subplot_cols, 5*num_subplot_rows))
-for index, col in enumerate(numerical_cols):
-    plt.subplot(num_subplot_rows, num_subplot_cols, num_subplot_cols*index+3, autoscale_on=True)
-    plt.boxplot(data[col].values)
-    plt.title(col)
-    plt.show()
+# =============================================================================
+# pd.set_option('display.max_columns', None)
+# 
+# statN=data.describe()
+# statC=data.describe(include=['object'])
+# 
+# display(data.info())
+# display(statN)
+# display(statC)
+# 
+# nrows = data.shape[0]
+# rcols = data.shape[1]
+# 
+# 
+# data5898=data.groupby("ETAT")["MIMAT0005898"]
+# data5951=data.groupby("ETAT")["MIMAT0005951"]
+# data19691=data.groupby("ETAT")["MIMAT0019691"]
+# data27623=data.groupby("ETAT")["MIMAT0027623"]
+# data27650=data.groupby("ETAT")["MIMAT0027650"]
+# 
+# display(data5898.describe())
+# display(data5951.describe())
+# display(data19691.describe())
+# display(data27623.describe())
+# display(data27650.describe())
+# 
+# 
+# numerical_cols = data.select_dtypes(exclude=['object']).columns
+# num_subplot_rows = len(numerical_cols)
+# num_subplot_cols = 5
+# plt.subplots(figsize=(5*num_subplot_cols, 5*num_subplot_rows))
+# for index, col in enumerate(numerical_cols):
+#     plt.subplot(num_subplot_rows, num_subplot_cols, num_subplot_cols*index+3, autoscale_on=True)
+#     plt.boxplot(data[col].values)
+#     plt.title(col)
+#     plt.show()
+#     
+#     
+# =============================================================================
+    
     
 # =============================================================================
 # BC5898=[]
